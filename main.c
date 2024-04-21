a708f7b0b801de92a48b10554c3ead9123d62734
lanjilepravin23 <lanjilepravin@gmail.com>
2024-04-22 00:02:57 +0100

    main to call the functions from LIS3MDLTR driver.

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: 21 Apr 2024
+ *      Author: pubu
+ */
+#include <stdio.h>
+#include "lis3mdltr.h"
+
+int main(){
+	uint8_t get_val;
+	status_t status;
+	status = lis3mdltr_init();
+	if (status == STATUS_OK)
+	{
+		printf("[DEBUG] LIS3MDLTR initialisation success.\n");
+	}
+	else{
+		printf("[ERROR] LIS3MDLTR initialisation failed.\n");
+		return 0;
+	}
+	get_val = lis3mdltr_GetFullScaleConfig();
+	return 0;
+}
